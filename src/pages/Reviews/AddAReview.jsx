8dff58a469c0758d5d10cdf9b44f48fabe09be77
developer-munir <devolopermunir@gmail.com>
2022-11-08 20:00:19 +0600

    get review data

--- src/pages/Reviews/AddAReview.jsx
+++ src/pages/Reviews/AddAReview.jsx
@@ -1,9 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthProvider";
+import './reviews.css'
+import { FaStar } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 const AddAReview = () => {
   const { user } = useContext(AuthContext);
+  const [number, setNumber] = useState(1);
 
+  const notify = () => toast("Cannot add review more than five star");
   const postReview = (e) => {
     e.preventDefault();
     const review = e.target.review.value;
@@ -13,12 +18,30 @@ const AddAReview = () => {
       photo: user?.photoURL,
       uid: user?.uid,
       review,
+      ratings: number,
     };
     console.log(reviewUserInfo);
   };
+  const ratingUser = () => {
+    if (number === 5) {
+      return notify();
+    }
+    setNumber(number + 1);
+  };
+  // reviews
   return (
     <div>
       <form className="text-center md:pr-3" onSubmit={postReview}>
+        <div
+          className="flex justify-between w-1/4 my-3 cursor-pointer"
+          onClick={ratingUser}
+        >
+          <FaStar></FaStar>
+          <FaStar></FaStar>
+          <FaStar></FaStar>
+          <FaStar></FaStar>
+          <FaStar></FaStar>
+        </div>
         <textarea
           className="textarea border border-black w-full"
           placeholder="Add review"
